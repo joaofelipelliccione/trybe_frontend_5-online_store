1d373e6ef7d63c0d1bb3af67ad4008a1cbdcdcd8
João Felipe P <jf.pelliccione@gmail.com>
2021-11-04 22:57:06 -0300

    Consertando refresh GH Pages - Tentativa 5.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     return(
       <footer>
         <span>
-          Developed by <a href="https://github.com/joaofelipelliccione" rel="noopener noreferrer" target="_blank">João Felipe Pelliccione</a> !
+          Developed by <a href="https://github.com/joaofelipelliccione" rel="noopener noreferrer" target="_blank">João Felipe Pelliccione</a>
         </span>
       </footer>
     );
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/trybe_frontend_5-online_store"> {/* Resolução do problema de reload Cart, no GH Pages. */}
+    <HashRouter basename="/trybe_frontend_5-online_store"> {/* Resolução do problema de reload Cart, no GH Pages. */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
