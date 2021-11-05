ec5dc86528a32d54222a7de031ef4162f62047ef
João Felipe P <jf.pelliccione@gmail.com>
2021-11-04 22:05:34 -0300

    Tentando consertar refresh GH-Pages (Tentativa 3).

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import ProductDetails from './pages/ProductDetails';
@@ -8,17 +8,15 @@ import ProductDetails from './pages/ProductDetails';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter basename="/trybe_frontend_5-online_store"> {/* Resolução do problema de reload Cart, no GH Pages. */}
-        <Switch>
-          <Route exact path="/" render={ () => <Home /> } />
-          <Route path="/cart" render={ () => <Cart /> } />
-          <Route
-            exact
-            path="/products/:id"
-            render={ (props) => <ProductDetails { ...props } /> }
-          />
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" render={ () => <Home /> } />
+        <Route path="/cart" render={ () => <Cart /> } />
+        <Route
+          exact
+          path="/products/:id"
+          render={ (props) => <ProductDetails { ...props } /> }
+        />
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename="/trybe_frontend_5-online_store"> {/* Resolução do problema de reload Cart, no GH Pages. */}
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
